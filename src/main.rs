e4b4478a8228336188da3d8c88af22d6f33e61db
AWPtimizer <luhemdha@gmail.com>
2024-04-30 13:00:08 +0530

    Assign Variables or Datatypes to variables

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    // this is how we initialize variables
+    let x: i8 = -5; // signed integer
+    let y: u32 = 77; // unsigned integer
+    let z: f32 = 1000.001; // float
+
+    // How we print the above variables:
+    // Method - 1 ->
+    print!("x: {}, y: {}, z: {} \n", x, y, z);
+
+    // Method - 2 ->
+    println!("x: {} ", x);
+    println!("y: {} ", y);
+    println!("z: {}", z);
 }
-    
